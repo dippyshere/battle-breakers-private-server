9d3c9c4d9525fc3b84699bedc44650b09dff636d
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2023-02-27 20:16:12 +1100

    add email + displayname lookup placeholder

--- api/account/publicAccount.py
+++ api/account/publicAccount.py
@@ -19,3 +19,25 @@ async def public_account_info(request):
     accountId = request.args.get("accountId")
     with open("res/account/api/public/account/ec0ebb7e56f6454e86c62299a7b32e21.json", "r", encoding='utf-8') as file:
         return sanic.response.json(orjson.loads(file.read()))
+
+
+@public_account.route("/account/api/public/account/displayName/<displayName>", methods=["GET"])
+async def account_displayname(request, displayName):
+    """
+    Get an account by name
+    :param request: The request object
+    :param displayName: The display name
+    :return: The response object
+    """
+    return sanic.response.text("")
+
+
+@public_account.route("/account/api/public/account/email/<email>", methods=["GET"])
+async def account_email(request, email):
+    """
+    Get account by email
+    :param request: The request object
+    :param email: The email
+    :return: The response object
+    """
+    return sanic.response.text("")
