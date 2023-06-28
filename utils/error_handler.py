4aa6d606f1f39bf8f3996cf6e1dc443d6fc5bc33
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2023-06-28 20:09:44 +1000

    add support for EpicException exception type in handling
    Signed-off-by: Alex Hanson <43489279+dippyshere@users.noreply.github.com>

--- utils/error_handler.py
+++ utils/error_handler.py
@@ -14,6 +14,8 @@ import uuid
 import sanic
 from sanic.handlers import ErrorHandler
 
+from .exceptions import EpicException
+
 
 class EpicError:
     """
@@ -39,48 +41,61 @@ class CustomErrorHandler(ErrorHandler):
     """
 
     def default(self, request: sanic.request.Request,
-                exception: sanic.exceptions.SanicException | Exception) -> sanic.response.JSONResponse:
+                exception: sanic.exceptions.SanicException | EpicException | Exception) -> sanic.response.JSONResponse:
         """
         Handles all exceptions
         :param request: The request object
         :param exception: The exception object
         :return: The response
         """
-        ErrorHandler.log(request, exception)
-        status_code = getattr(exception, "status_code", 500)
-        message_vars = []
-        for var in getattr(exception, "args", []):
-            message_vars.append(var)
-        error = error_details.get(status_code, {
-            "error_code": "errors.com.epicgames.unknown_error.v2",
-            "error_message": "An unknown error has occurred, please report to admin. 🗿",
-            "numeric_error_code": 0
-        })
-        error_code = error["error_code"]
-        error_message = error["error_message"]
-        numeric_error_code = error["numeric_error_code"]
-        if getattr(exception, "context", None) is not None:
-            error_code = exception.context.get("errorCode", error_code)
-            error_message = exception.context.get("errorMessage", error_message)
-            numeric_error_code = exception.context.get("numericErrorCode", numeric_error_code)
-        if True:  # request.app.debug
-            if status_code == 500:
-                error_message = ""
-                for message in message_vars:
-                    error_message += f"{message}"
-            error_message += f" ({exception.__class__.__name__})"
-        headers = {
-            "X-Epic-Error-Code": numeric_error_code,
-            "X-Epic-Error-Name": error_code,
-        }
-        return sanic.response.json({
-            "errorCode": error_code,
-            "errorMessage": error_message,
-            "messageVars": message_vars,
-            "numericErrorCode": numeric_error_code,
-            "originatingService": "WEX",
-            "intent": "prod",
-            "trackingId": uuid.uuid4().__str__(),
-            "errorDescription": error_message,
-            "errorName": error_code.split(".")[-1],
-        }, status_code, headers)
+        if isinstance(exception, EpicException):
+            exception_dict = exception.as_dict()
+            status_code = getattr(exception, "statusCode", 500)
+            headers = {
+                "X-Epic-Error-Code": exception_dict["numericErrorCode"],
+                "X-Epic-Error-Name": exception_dict["errorCode"],
+            }
+            ErrorHandler.log(request, exception)
+            if request.app.debug:
+                exception_dict["errorMessage"] += f" ({exception.__class__.__name__})"
+            exception_dict["errorMessage"] += f" ({exception.__class__.__name__})"
+            return sanic.response.json(exception_dict, status=status_code, headers=headers)
+        else:
+            ErrorHandler.log(request, exception)
+            status_code = getattr(exception, "status_code", 500)
+            message_vars = []
+            for var in getattr(exception, "args", []):
+                message_vars.append(var)
+            error = error_details.get(status_code, {
+                "error_code": "errors.com.epicgames.unknown_error.v2",
+                "error_message": "An unknown error has occurred, please report to admin. 🗿",
+                "numeric_error_code": 0
+            })
+            error_code = error["error_code"]
+            error_message = error["error_message"]
+            numeric_error_code = error["numeric_error_code"]
+            if getattr(exception, "context", None) is not None:
+                error_code = exception.context.get("errorCode", error_code)
+                error_message = exception.context.get("errorMessage", error_message)
+                numeric_error_code = exception.context.get("numericErrorCode", numeric_error_code)
+            if True:  # request.app.debug
+                if status_code == 500:
+                    error_message = ""
+                    for message in message_vars:
+                        error_message += f"{message}"
+                error_message += f" ({exception.__class__.__name__})"
+            headers = {
+                "X-Epic-Error-Code": numeric_error_code,
+                "X-Epic-Error-Name": error_code,
+            }
+            return sanic.response.json({
+                "errorCode": error_code,
+                "errorMessage": error_message,
+                "messageVars": message_vars,
+                "numericErrorCode": numeric_error_code,
+                "originatingService": "WEX",
+                "intent": "prod",
+                "trackingId": uuid.uuid4().__str__(),
+                "errorDescription": error_message,
+                "errorName": error_code.split(".")[-1],
+            }, status_code, headers)
