216047643e26161d86b6a0c0fadc4abccb269153
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2023-07-13 02:47:52 +1000

    add equip/unequip for sidekicks
    Signed-off-by: Alex Hanson <43489279+dippyshere@users.noreply.github.com>

--- api/wex/profile/modify_hero_gear.py
+++ api/wex/profile/modify_hero_gear.py
@@ -9,6 +9,7 @@ Handles modifying hero gear.
 
 import sanic
 
+from utils.enums import ProfileType
 from utils.exceptions import errors
 from utils.utils import authorized as auth
 
@@ -19,6 +20,7 @@ wex_profile_modify_hero_gear = sanic.Blueprint("wex_profile_modify_hero_gear")
 
 
 # https://github.com/dippyshere/battle-breakers-documentation/blob/main/docs/World%20Explorers%20Service/wex/api/game/v2/profile/accountId/ModifyHeroGear.md
+# noinspection IncorrectFormatting
 @wex_profile_modify_hero_gear.route("/<accountId>/ModifyHeroGear", methods=["POST"])
 @auth(strict=True)
 @compress.compress()
@@ -29,4 +31,45 @@ async def modify_hero_gear(request: sanic.request.Request, accountId: str) -> sa
     :param accountId: The account id
     :return: The modified profile
     """
-    raise errors.com.epicgames.not_implemented()
+    if request.json.get("bIsInPit"):
+        if not (await request.ctx.profile.get_item_by_guid(request.json.get("heroItemId"), ProfileType.MONSTERPIT))[
+                "templateId"].startswith("Character:"):
+            raise errors.com.epicgames.world_explorers.bad_request(errorMessage="Invalid hero item id")
+    else:
+        if not (await request.ctx.profile.get_item_by_guid(request.json.get("heroItemId")))[
+                "templateId"].startswith("Character:"):
+            raise errors.com.epicgames.world_explorers.bad_request(errorMessage="Invalid hero item id")
+    if request.json.get("gearHeroItemId") != "":
+        sidekick_template_id = (await request.ctx.profile.get_item_by_guid(request.json.get("gearHeroItemId")))[
+                "templateId"]
+        if not sidekick_template_id.startswith("Character:"):
+            raise errors.com.epicgames.world_explorers.bad_request(errorMessage="Invalid gear weapon item id")
+        await request.ctx.profile.change_item_attribute(request.json.get("gearHeroItemId"), "used_as_sidekick", True)
+        await request.ctx.profile.change_item_attribute(request.json.get("gearHeroItemId"), "sidekick_item_id",
+                                                        request.json.get("heroItemId"))
+        if request.json.get("bIsInPit"):
+            await request.ctx.profile.change_item_attribute(request.json.get("heroItemId"), "sidekick_item_id",
+                                                            request.json.get("gearHeroItemId"),
+                                                            ProfileType.MONSTERPIT)
+            await request.ctx.profile.change_item_attribute(request.json.get("heroItemId"), "sidekick_template_id",
+                                                            sidekick_template_id, ProfileType.MONSTERPIT)
+        else:
+            await request.ctx.profile.change_item_attribute(request.json.get("heroItemId"), "sidekick_item_id",
+                                                            request.json.get("gearHeroItemId"))
+            await request.ctx.profile.change_item_attribute(request.json.get("heroItemId"), "sidekick_template_id",
+                                                            sidekick_template_id)
+    else:
+        await request.ctx.profile.change_item_attribute(request.json.get("gearHeroItemId"), "used_as_sidekick", False)
+        await request.ctx.profile.change_item_attribute(request.json.get("gearHeroItemId"), "sidekick_item_id", "")
+        if request.json.get("bIsInPit"):
+            await request.ctx.profile.change_item_attribute(request.json.get("heroItemId"), "sidekick_item_id", "",
+                                                            ProfileType.MONSTERPIT)
+            await request.ctx.profile.change_item_attribute(request.json.get("heroItemId"), "sidekick_template_id",
+                                                            "", ProfileType.MONSTERPIT)
+        else:
+            await request.ctx.profile.change_item_attribute(request.json.get("heroItemId"), "sidekick_item_id", "")
+            await request.ctx.profile.change_item_attribute(request.json.get("heroItemId"), "sidekick_template_id", "")
+    return sanic.response.json(
+        await request.ctx.profile.construct_response(request.ctx.profile_id, request.ctx.rvn,
+                                                     request.ctx.profile_revisions, True)
+    )
