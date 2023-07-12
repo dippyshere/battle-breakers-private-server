a08524880de927fe09f40966548406f8f06faf19
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2023-07-12 23:44:42 +1000

    add hero sell (pit copy upgrade not entirely complete)
    Signed-off-by: Alex Hanson <43489279+dippyshere@users.noreply.github.com>

--- api/wex/profile/sell_hero.py
+++ api/wex/profile/sell_hero.py
@@ -9,6 +9,7 @@ Handles selling heroes.
 
 import sanic
 
+from utils.enums import ProfileType
 from utils.exceptions import errors
 from utils.utils import authorized as auth
 
@@ -29,4 +30,78 @@ async def sell_hero(request: sanic.request.Request, accountId: str) -> sanic.res
     :param accountId: The account id
     :return: The modified profile
     """
-    raise errors.com.epicgames.not_implemented()
+    # TODO: validation
+    if request.json.get("bIsInPit"):
+        # hero_item = await request.ctx.profile.get_item_by_guid(request.json.get("heroItemId"), ProfileType.MONSTERPIT)
+        raise errors.com.epicgames.world_explorers.bad_request(errorMessage="How did you do this?")
+    else:
+        hero_item = await request.ctx.profile.get_item_by_guid(request.json.get("heroItemId"))
+    if not hero_item.get("templateId").startswith("Character:"):
+        raise errors.com.epicgames.world_explorers.bad_request(errorMessage="Invalid character item id")
+    hero_data = await request.app.ctx.load_character_data(hero_item["templateId"])
+    sell_rewards = (await request.app.ctx.load_datatable(
+        hero_data[0]["Properties"]["SellRewards"]["AssetPathName"].replace("/Game/", "Content/").split(".")[0]))[0][
+        "Properties"]["RequiredItems"]
+    foil_sell_rewards = (await request.app.ctx.load_datatable(
+        hero_data[0]["Properties"]["FoilSellRewards"]["AssetPathName"].replace("/Game/", "Content/").split(".")[0]))[0][
+        "Properties"]["RequiredItems"]
+    pit_unlocks = await request.ctx.profile.find_item_by_template_id("MonsterPitUnlock:Character",
+                                                                     ProfileType.MONSTERPIT)
+    # TODO: make this work with evolution alterations
+    pit_copy_guid = await request.ctx.profile.find_item_by_template_id(hero_item["templateId"], ProfileType.MONSTERPIT)
+    if pit_copy_guid:
+        pit_copy = await request.ctx.profile.get_item_by_guid(pit_copy_guid[0], ProfileType.MONSTERPIT)
+        if pit_copy["attributes"]["foil_lvl"] < 0 < hero_item["attributes"]["foil_lvl"]:
+            await request.ctx.profile.change_item_attribute(pit_copy_guid[0], "foil_lvl",
+                                                            hero_item["attributes"]["foil_lvl"], ProfileType.MONSTERPIT)
+        elif pit_copy["attributes"]["foil_lvl"] > 0 and hero_item["attributes"]["foil_lvl"] > 0:
+            for foil_reward in foil_sell_rewards:
+                reward_template_id = await request.app.ctx.get_template_id_from_path(
+                    foil_reward["ItemDefinition"]["ObjectPath"])
+                current_item = await request.ctx.profile.find_item_by_template_id(reward_template_id)
+                if current_item:
+                    current_quantity = (await request.ctx.profile.get_item_by_guid(current_item[0]))["quantity"]
+                    await request.ctx.profile.change_item_quantity(current_item[0],
+                                                                   current_quantity + foil_reward["Count"])
+                else:
+                    await request.ctx.profile.add_item({
+                        "templateId": reward_template_id,
+                        "attributes": {},
+                        "quantity": foil_reward["Count"]
+                    })
+    for pit_unlock_guid in pit_unlocks:
+        pit_unlock = await request.ctx.profile.get_item_by_guid(pit_unlock_guid, ProfileType.MONSTERPIT)
+        if pit_unlock["attributes"]["characterId"] == hero_item["templateId"]:
+            await request.ctx.profile.change_item_attribute(pit_unlock_guid, "num_sold",
+                                                            pit_unlock["attributes"]["num_sold"] + 1,
+                                                            ProfileType.MONSTERPIT)
+            break
+    else:
+        await request.ctx.profile.add_item({
+            "templateId": "MonsterPitUnlock:Character",
+            "attributes": {
+                "num_sold": 1,
+                "characterId": hero_item["templateId"]
+            },
+            "quantity": 1
+        }, ProfileType.MONSTERPIT)
+    for sell_reward in sell_rewards:
+        reward_template_id = await request.app.ctx.get_template_id_from_path(
+            sell_reward["ItemDefinition"]["ObjectPath"])
+        current_item = await request.ctx.profile.find_item_by_template_id(reward_template_id)
+        if current_item:
+            current_quantity = (await request.ctx.profile.get_item_by_guid(current_item[0]))["quantity"]
+            await request.ctx.profile.change_item_quantity(current_item[0],
+                                                           current_quantity + sell_reward["Count"])
+        else:
+            await request.ctx.profile.add_item({
+                "templateId": reward_template_id,
+                "attributes": {},
+                "quantity": sell_reward["Count"]
+            })
+    await request.ctx.profile.remove_item(request.json.get("heroItemId"))
+    return sanic.response.json(
+        await request.ctx.profile.construct_response(request.ctx.profile_id, request.ctx.rvn,
+                                                     request.ctx.profile_revisions)
+    )
+
