aa6649dbe0810a27c84c7be2e11ae25bc7501163
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2023-07-21 00:02:59 +1000

    fix enabled features
    Signed-off-by: Alex Hanson <43489279+dippyshere@users.noreply.github.com>

--- api/wex/enabled_features.py
+++ api/wex/enabled_features.py
@@ -18,11 +18,10 @@ wex_enabled_features = sanic.Blueprint("wex_enabled_features")
 # https://github.com/dippyshere/battle-breakers-documentation/blob/main/docs/World%20Explorers%20Service/wex/api/v2/versioncheck/Windows.md
 @wex_enabled_features.route("/api/game/v2/enabled_features", methods=['GET'])
 @compress.compress()
-async def enabled_features(request: sanic.request.Request, platform: str) -> sanic.response.JSONResponse:
+async def enabled_features(request: sanic.request.Request) -> sanic.response.JSONResponse:
     """
     Handles the enabled features check.
     :param request: The request object
-    :param platform: The platform
     :return: The response object
     """
     # Unsure what could/should be here, this feature was disabled via config files after 1.80 on pc
