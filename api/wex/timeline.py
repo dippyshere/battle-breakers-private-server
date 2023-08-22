00f80e2e2a4c7f9e5e4dba0ea7dd7851768d8a17
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2023-08-23 00:00:48 +1000

    temporary timeline edits
    Signed-off-by: Alex Hanson <43489279+dippyshere@users.noreply.github.com>

--- api/wex/timeline.py
+++ api/wex/timeline.py
@@ -31,10 +31,91 @@ async def calendar(request: sanic.request.Request) -> sanic.response.JSONRespons
     # with open("res/wex/api/calendar/v1/timeline.json", "r", encoding='utf-8') as file:
     #     return sanic.response.json(orjson.loads(file.read()))
     # return await sanic.response.file_stream("res/wex/api/calendar/v1/timeline.json", mime_type="application/json")
+    valid_from = await request.app.ctx.format_time(
+        (datetime.datetime.utcnow() - datetime.timedelta(hours=1)))
+    cache_expire = await request.app.ctx.format_time(
+        (datetime.datetime.utcnow() + datetime.timedelta(hours=2)))
+    calendar_channels = {
+        "news": {
+            "states": [
+                {
+                    "validFrom": valid_from,
+                    "activeEvents": [],
+                    "state": {}
+                }
+            ],
+            "cacheExpire": cache_expire
+        },
+        "limited-time-mode": {
+            "states": [
+                {
+                    "validFrom": valid_from,
+                    "activeEvents": [],
+                    "state": {}
+                }
+            ],
+            "cacheExpire": cache_expire
+        },
+        "marketing": {
+            "states": [
+                {
+                    "validFrom": valid_from,
+                    "activeEvents": [],
+                    "state": {}
+                }
+            ],
+            "cacheExpire": cache_expire
+        },
+        "rotational-content": {
+            "states": [
+                {
+                    "validFrom": valid_from,
+                    "activeEvents": [],
+                    "state": {}
+                }
+            ],
+            "cacheExpire": cache_expire
+        },
+        "featured-stores-mcp": {
+            "states": [
+                {
+                    "validFrom": valid_from,
+                    "activeEvents": [],
+                    "state": {}
+                }
+            ],
+            "cacheExpire": cache_expire
+        },
+        "weekly-challenge": {
+            "states": [
+                {
+                    "validFrom": valid_from,
+                    "activeEvents": [],
+                    "state": {}
+                }
+            ],
+            "cacheExpire": cache_expire
+        },
+        "battlepass": {
+            "states": [
+                {
+                    "validFrom": valid_from,
+                    "activeEvents": [],
+                    "state": {}
+                }
+            ],
+            "cacheExpire": cache_expire
+        }
+    }
     timeline = await request.app.ctx.read_file("res/wex/api/calendar/v1/timeline.json")
-    timeline["currentTime"] = await request.app.ctx.format_time()
     for channel in timeline["channels"]:
         for state in timeline["channels"][channel]["states"]:
             state["validFrom"] = await request.app.ctx.format_time((datetime.datetime.utcnow() - datetime.timedelta(hours=1)))
         timeline["channels"][channel]["cacheExpire"] = await request.app.ctx.format_time((datetime.datetime.utcnow() + datetime.timedelta(hours=2)))
+    # return sanic.response.json({
+    #     "channels": calendar_channels,
+    #     "eventsTimeOffsetHrs": 0.0,
+    #     "cacheIntervalMins": 15.0,
+    #     "currentTime": await request.app.ctx.format_time()
+    # })
     return sanic.response.json(timeline)
