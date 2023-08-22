becbaefd8b6e01028f0c130f26e479ef8ebb7f19
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2023-08-23 01:52:35 +1000

    implement friend suggestions
    Signed-off-by: Alex Hanson <43489279+dippyshere@users.noreply.github.com>

--- api/wex/profile/suggest_friends.py
+++ api/wex/profile/suggest_friends.py
@@ -6,11 +6,14 @@ This code is licensed under the [TBD] license.
 
 Handles the friend suggestion request
 """
+import datetime
 
 import sanic
 
-from utils.exceptions import errors
-from utils.utils import authorized as auth
+from utils.enums import ProfileType
+from utils.friend_system import PlayerFriends
+from utils.profile_system import PlayerProfile
+from utils.utils import authorized as auth, read_file, format_time
 
 from utils.sanic_gzip import Compress
 
@@ -29,4 +32,63 @@ async def suggest_friends(request: sanic.request.Request, accountId: str) -> san
     :param accountId: The account id
     :return: The modified profile
     """
-    raise errors.com.epicgames.not_implemented()
+    if accountId not in request.app.ctx.friends:
+        request.app.ctx.friends[accountId] = PlayerFriends(accountId)
+    suggested_accounts = await request.app.ctx.friends[accountId].suggest_friends(request)
+    for account in suggested_accounts:
+        account_data = await read_file(f"res/account/api/public/account/{account}.json")
+        if account not in request.app.ctx.profiles:
+            request.app.ctx.profiles[account] = PlayerProfile(account)
+        wex_data = await request.app.ctx.profiles[account].get_profile(ProfileType.PROFILE0)
+        rep_heroes = []
+        account_perks = []
+        for account_perk in ["MaxHitPoints", "RegenStat", "PetStrength", "BasicAttack", "Attack", "SpecialAttack",
+                             "DamageReduction", "MaxMana"]:
+            account_perks.append(wex_data["stats"]["attributes"].get("account_perks").get(account_perk, 0))
+        for hero_id in wex_data["stats"]["attributes"].get("rep_hero_ids", []):
+            hero_data = await request.app.ctx.profiles[account].get_item_by_guid(hero_id)
+            rep_heroes.append({
+                "itemId": hero_id,
+                "templateId": hero_data.get("templateId"),
+                "bIsCommander": True,
+                "level": hero_data.get("attributes").get("level"),
+                "skillLevel": hero_data.get("attributes").get("skill_level"),
+                "upgrades": hero_data.get("attributes").get("upgrades"),
+                "accountInfo": {
+                    "level": wex_data["stats"]["attributes"].get("level", 0),
+                    "perks": account_perks
+                },
+                "foilLevel": hero_data.get("attributes").get("foil_lvl", -1),
+                "gearTemplateId": hero_data.get("attributes").get("sidekick_template_id", ""),
+            })
+        await request.ctx.profile.add_item({
+            "templateId": "Friend:Instance",
+            "attributes": {
+                "lifetime_claimed": 0,
+                "accountId": account_data["id"],
+                "canBeSparred": False,
+                "snapshot_expires": await format_time(
+                    datetime.datetime.utcnow() + datetime.timedelta(hours=3)),
+                "best_gift": 0,  # These stats are unique to the friend instance on the profile, not the friend
+                "lifetime_gifted": 0,
+                "snapshot": {
+                    "displayName": account_data["displayName"],
+                    "avatarUrl": "wex-temp-avatar.png",
+                    "repHeroes": rep_heroes,
+                    "lastPlayTime": wex_data["updated"],
+                    "numLevelsCompleted": wex_data["stats"]["attributes"].get("num_levels_completed", 0),
+                    "numTerritoriesClaimed": wex_data["stats"]["attributes"].get("num_territories_claimed", 0),
+                    "accountLevel": wex_data["stats"]["attributes"].get("level", 0),
+                    "numRepHeroes": len(wex_data["stats"]["attributes"].get("rep_hero_ids", [])),
+                    "isPvPUnlocked": wex_data["stats"]["attributes"].get("is_pvp_unlocked", False)
+                },
+                "remoteFriendId": account_data["id"],
+                "status": "Suggested",
+                "gifts": {}
+            },
+            "quantity": 1
+        }, profile_id=ProfileType.FRIENDS)
+    return sanic.response.json(
+        await request.ctx.profile.construct_response(request.ctx.profile_id, request.ctx.rvn,
+                                                     request.ctx.profile_revisions)
+    )
